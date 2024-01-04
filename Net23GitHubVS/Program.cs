4c7abeaffc02f65955db2f688bbe39fbe52a504c
Reidar Nilsen <4reidar@gmail.com>
2024-01-04 09:33:14 +0100

    Add project files.

---
+++ Net23GitHubVS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Net23GitHubVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
