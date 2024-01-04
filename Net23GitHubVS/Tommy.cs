3d51ee50c141e8f7a401457dc1796e2690d78b7c
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-01-04 09:43:13 +0100

    Lagt till en greeting

--- Net23GitHubVS/Tommy.cs
+++ Net23GitHubVS/Tommy.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Net23GitHubVS
 {
     internal class Tommy
     {
+        public void greeting()
+        {
+            Console.WriteLine("Hej");
+        }
+       
     }
 }
